90a015e26a233cd1e3991be5a5c5ddd2df5c789c
chiancw <chiancw@gmail.com>
2025-06-09 14:07:51 +0800

    first draft

---
+++ main.tf
@@ -0,0 +1,68 @@
+data "azapi_client_config" "current" {}
+
+provider "alz" {
+  library_overwrite_enabled = true
+  library_references = [
+    {
+      path = "platform/amba"
+      ref  = "2025.05.0"
+    },
+    {
+      custom_url = "${path.root}/lib"
+    }
+  ]
+}
+
+provider "azurerm" {
+  alias           = "management"
+  subscription_id = var.management_subscription_id != "" ? var.management_subscription_id : data.azapi_client_config.current.subscription_id
+  features {}
+}
+
+locals {
+  root_management_group_name = jsondecode(file("${path.root}/lib/custom.alz_architecture_definition.json")).management_groups[0].id
+}
+
+module "amba_alz" {
+  source  = "Azure/avm-ptn-monitoring-amba-alz/azurerm"
+  version = "0.1.1"
+  providers = {
+    azurerm = azurerm.management
+  }
+  count = var.bring_your_own_user_assigned_managed_identity ? 0 : 1
+
+  location                            = var.location
+  root_management_group_name          = local.root_management_group_name
+  resource_group_name                 = var.resource_group_name
+  tags                                = var.tags
+  user_assigned_managed_identity_name = var.user_assigned_managed_identity_name
+}
+
+module "amba_policy" {
+  source  = "Azure/avm-ptn-alz/azurerm"
+  version = "0.12.0"
+
+  architecture_name  = "custom"
+  location           = var.location
+  parent_resource_id = data.azapi_client_config.current.tenant_id
+  policy_default_values = {
+    amba_alz_management_subscription_id            = jsonencode({ value = var.management_subscription_id != "" ? var.management_subscription_id : data.azapi_client_config.current.subscription_id })
+    amba_alz_resource_group_location               = jsonencode({ value = var.location })
+    amba_alz_resource_group_name                   = jsonencode({ value = var.resource_group_name })
+    amba_alz_resource_group_tags                   = jsonencode({ value = var.tags })
+    amba_alz_user_assigned_managed_identity_name   = jsonencode({ value = var.user_assigned_managed_identity_name })
+    amba_alz_byo_user_assigned_managed_identity_id = jsonencode({ value = var.bring_your_own_user_assigned_managed_identity_resource_id })
+    amba_alz_disable_tag_name                      = jsonencode({ value = var.amba_disable_tag_name })
+    amba_alz_disable_tag_values                    = jsonencode({ value = var.amba_disable_tag_values })
+    amba_alz_action_group_email                    = jsonencode({ value = var.action_group_email })
+    amba_alz_arm_role_id                           = jsonencode({ value = var.action_group_arm_role_id })
+    amba_alz_webhook_service_uri                   = jsonencode({ value = var.webhook_service_uri })
+    amba_alz_event_hub_resource_id                 = jsonencode({ value = var.event_hub_resource_id })
+    amba_alz_function_resource_id                  = jsonencode({ value = var.function_resource_id })
+    amba_alz_function_trigger_url                  = jsonencode({ value = var.function_trigger_uri })
+    amba_alz_logicapp_resource_id                  = jsonencode({ value = var.logic_app_resource_id })
+    amba_alz_logicapp_callback_url                 = jsonencode({ value = var.logic_app_callback_url })
+    amba_alz_byo_alert_processing_rule             = jsonencode({ value = var.bring_your_own_alert_processing_rule_resource_id })
+    amba_alz_byo_action_group                      = jsonencode({ value = var.bring_your_own_action_group_resource_id })
+  }
+}
